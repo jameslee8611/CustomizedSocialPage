a96dee160dda7e3a9e303d5558b9203d05cdd025
jysong14 <song14@illinois.edu>
2014-06-24 14:01:34 -0500

    add forget page & reset page

---
+++ views/forget/reset.php
@@ -0,0 +1,7 @@
+<div>
+	<form action="" method="post">
+		<label>New Password<br></label><input type="password"/><br>
+		<label>Confirm Password<br></label><input type="password"/><br>
+		<input type="submit" value="Submit"/>
+	</form>
+</div>
