2cb9a8088492a36d079f765ccce20c432ec2178d
yjw9012 <yjw9012@hotmail.com>
2014-07-05 14:59:33 -0500

    Modified dbScript.php

--- config/private/dbScript.php
+++ config/private/dbScript.php
@@ -6,6 +6,18 @@
  * and open the template in the editor.
  */
 
+require '../../libs/Router.php';
+require '../../libs/Controller.php';
+require '../../libs/View.php';
+require '../../libs/Model.php';
+
+//Library
+require '../../libs/Database.php';
+require '../../libs/Session.php';
+
+require '../constant.php';
+require '../database.php';
+
 $db = new Database();
 $query = 
 "CREATE TABLE users
