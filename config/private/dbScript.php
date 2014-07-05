78c9f2e8fbe34f9ab8927889541234b86ddc17b3
yjw9012 <yjw9012@hotmail.com>
2014-07-05 14:46:58 -0500

    Created a dbScript

--- config/private/dbScript.php
+++ config/private/dbScript.php
@@ -6,3 +6,22 @@
  * and open the template in the editor.
  */
 
+$db = new Database();
+$query = 
+"CREATE TABLE users
+(
+	id int NOT NULL,
+	username varchar(25) NOT NULL,
+	email varchar(32) NOT NULL,
+	password varchar(32) NOT NULL,
+	reset varchar(32) default NULL
+);";
+
+$statement = $db->prepare($query);
+$success = $statement->execute();
+
+if($success)
+    echo 'DBs have been successfully created.';
+else echo 'DBs failed to be created.';  
+
+?>
