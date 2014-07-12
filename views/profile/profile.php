d2bf785235d50784023cd9fd75fff3c05896d7a2
Seungchul Lee <sclee8611@gmail.com>
2014-07-11 23:44:41 -0500

    merge

--- views/profile/profile.php
+++ views/profile/profile.php
@@ -1,3 +1,8 @@
+<!--
+    @Author : Seungchul Lee
+    @Date   : June 24, 2014
+-->
+
 <div class="header">
     <div class="row">
         <div class="large-1 columns">
@@ -16,27 +21,32 @@
 <div class="content">
 
     <div class="row">
-        <div class="large-12 columns">
-
-            <div class="row">
-                <div class="large-12 hide-for-small">
-
-                    <div id="featured" data-orbit>
-                        <img src="http://placehold.it/1200x500&text=Slide Image 1" alt="slide image">
-                        <img src="http://placehold.it/1200x500&text=Slide Image 2" alt="slide image">
-                        <img src="http://placehold.it/1200x500&text=Slide Image 3" alt="slide image">
+        <div class="large-3 columns ">
+            <div class="panel">
+                <a href="#"><img src="http://placehold.it/300x240&text=[img]"/></a>
+                <h4><a href="<?php echo URL . Session::get('username'); ?>"><?php echo Session::get('username'); ?></a></h5>
+                    <div class="section-container side-nav" data-section data-options="deep_linking: false; one_up: true">
+                        <section class="section">
+                            <h6 style="font-size: 15px;" class="title"><a href="<?php echo URL; ?>setting">Setting</a></h6>
+                        </section>
+                        <section class="divider">
+                            
+                        </section>
+                        <section class="title">
+                            <h6 style="font-size: 15px;" class="title"><a href="#">Status</a></h6>
+                        </section>
+                        <section class="title">
+                            <h6 style="font-size: 15px;" class="title"><a href="#">Pictures</a></h6>
+                        </section>
+                        <section class="title">
+                            <h6 style="font-size: 15px;" class="title"><a href="#">Videos</a></h6>
+                        </section>
                     </div>
 
-                </div>
             </div>
-            <div class="row">
-                <div class="large-12 columns show-for-small">
+        </div>
 
-                    <img src="http://placehold.it/1200x700&text=Mobile Header">
-
-                </div>
-            </div><br>
-            
+        <div class="large-6 columns">
             <div class="row">
                 <div class="large-12 columns">
                     <form class="post-form">
@@ -68,114 +78,98 @@
                 </div>
             </div><br>
             <div class="row">
-                <div class="large-6 columns">
-                    <?php echo $this->post; ?>
-                </div>
-                <div class="large-6 columns">
-                    <?php echo $this->comment; ?>
-                </div>
-            </div><br>
+                <div class="large-2 columns small-3"><img src="http://placehold.it/80x80&text=[img]"/></div>
+                <div class="large-10 columns">
+                    <p><strong>Some Person said:</strong> Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong.</p>
+                    <ul class="inline-list">
+                        <li><a href="">Reply</a></li>
+                        <li><a href="">Share</a></li>
+                    </ul>
 
-            <div class="row">
-                <div class="large-12 columns">
+
+                    <h6>2 Comments</h6>
                     <div class="row">
-
-                        <div class="large-4 small-6 columns">
-
-                            <h4>Upcoming Shows</h4><hr>
-
-                            <div class="row">
-                                <div class="large-1 column">
-                                    <img src="http://placehold.it/50x50&text=[img]">
-                                </div>
-
-                                <div class="large-9 columns">
-                                    <h5><a href="#">Venue Name</a></h5>
-                                    <h6 class="subheader show-for-small">Doors at 00:00pm</h6>
-                                </div>
-                            </div><hr>
-
-                            <div class="hide-for-small">
-                                <div class="row">
-                                    <div class="large-1 column">
-                                        <img src="http://placehold.it/50x50&text=[img]">
-                                    </div>
-
-                                    <div class="large-9 columns">
-                                        <h5 class="subheader"><a href="#">Venue Name</a></h5>
-                                    </div>
-                                </div><hr>
-
-                                <div class="row">
-                                    <div class="large-1 column">
-                                        <img src="http://placehold.it/50x50&text=[img]">
-                                    </div>
-
-                                    <div class="large-9 columns">
-                                        <h5 class="subheader"><a href="#">Venue Name</a></h5>
-                                    </div>
-                                </div><hr>
-
-                                <div class="row">
-                                    <div class="large-1 column">
-                                        <img src="http://placehold.it/50x50&text=[img]">
-                                    </div>
-
-                                    <div class="large-9 columns">
-                                        <h5 class="subheader"><a href="#">Venue Name</a></h5>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="large-4 small-6 columns">
-                            <img src="http://placehold.it/300x465&text=Image">
-                        </div>
-
-                        <div class="large-4 small-12 columns">
-
-                            <h4>Blog</h4><hr>
-                            <div class="panel">
-                                <h5><a href="#">Post Title 1</a></h5>
-
-                                <h6 class="subheader">
-                                    Risus ligula, aliquam nec fermentum vitae, sollicitudin eget urna. Suspendisse ultrices ornare tempor...
-                                </h6>
-
-                                <h6><a href="#">Read More »</a></h6>
-                            </div>
-
-                            <div class="panel hide-for-small">
-                                <h5><a href="#">Post Title 2 »</a></h5>
-                            </div>
-
-                            <div class="panel hide-for-small">
-                                <h5><a href="#">Post Title 3 »</a></h5>
-                            </div>
-
-                            <a href="#" class="right">Go To Blog »</a>
-
-                        </div>
-
+                        <div class="large-2 columns small-3"><img src="http://placehold.it/50x50&text=[img]"/></div>
+                        <div class="large-10 columns"><p>Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit</p></div>
+                    </div>
+                    <div class="row">
+                        <div class="large-2 columns small-3"><img src="http://placehold.it/50x50&text=[img]"/></div>
+                        <div class="large-10 columns"><p>Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit</p></div>
                     </div>
                 </div>
             </div>
 
+
+            <hr/>
+
+
+            <div class="row">
+                <div class="large-2 columns small-3"><img src="http://placehold.it/80x80&text=[img]"/></div>
+                <div class="large-10 columns">
+                    <p><strong>Some Person said:</strong> Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong.</p>
+                    <ul class="inline-list">
+                        <li><a href="">Reply</a></li>
+                        <li><a href="">Share</a></li>
+                    </ul>
+                </div>
+            </div>
+
+
+            <hr/>
+
+
+            <div class="row">
+                <div class="large-2 columns small-3"><img src="http://placehold.it/80x80&text=[img]"/></div>
+                <div class="large-10 columns">
+                    <p><strong>Some Person said:</strong> Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong.</p>
+                    <ul class="inline-list">
+                        <li><a href="">Reply</a></li>
+                        <li><a href="">Share</a></li>
+                    </ul>
+
+
+                    <h6>2 Comments</h6>
+                    <div class="row">
+                        <div class="large-2 columns small-3"><img src="http://placehold.it/50x50&text=[img]"/></div>
+                        <div class="large-10 columns"><p>Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit</p></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <aside class="large-3 columns hide-for-small">
+            <p><img src="http://placehold.it/300x440&text=[ad]"/></p>
+            <p><img src="http://placehold.it/300x440&text=[ad]"/></p>
+        </aside>
+
+    </div>
+</div>
+
+<div class="header">
+    <div class="row">
+        <div class="large-1 columns">
+            <div class="row">
+                <div class="large-2 columns">
+                    <a href="<?php echo URL; ?>">Index</a>
+                </div>
+                <div class="large-2 columns">
+                    <a href="<?php echo URL; ?>index/logout">Logout</a>
+                </div>
+            </div>
         </div>
     </div>
-
 </div>
+
 <script>
     $(document).foundation();
-    $('.tab-title').click(function(){
-        if($(this).hasClass('active')){
+    $('.tab-title').click(function() {
+        if ($(this).hasClass('active')) {
             var deact_target = $(this).children().attr('href')
             $(this).removeClass('active');
             $(deact_target).removeClass('active');
             return false;
         }
     });
-    $('#post-textarea').click(function(){
+    $('#post-textarea').click(function() {
         $('#post-friends').show();
     });
 </script>
